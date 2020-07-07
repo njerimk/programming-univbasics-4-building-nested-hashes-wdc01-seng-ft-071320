d23ea11a0a1febf90fec5360837f80fb927bef98
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 18:34:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,20 @@
 def base_hash
-  anime ={}
-  anime = {
+  hash ={}
+  hash = {
     :railroads =>{}
   }
-    anime
+  hash
 end
  base_hash
 
 def monopoly_with_second_tier
+  hash ={}
+  hash ={
+    :railroads =>{},
+    :pieces => 4
+  }
+  
+  
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
